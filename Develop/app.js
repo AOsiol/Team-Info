70ea0a5c100b814352288996c425af92e877ea9f
Anthony Osiol <funnynoun@gmail.com>
2020-04-26 14:15:38 -0400

    fixed issue with collecting answers, cleaned up code and wrote readme

--- Develop/app.js
+++ Develop/app.js
@@ -3,29 +3,38 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs").promises;
+const fs = require("fs");
 
 const render = require("./lib/htmlRenderer");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-let employees = [];
+const employees = [];
+
+const introQuestion = [
+  {
+    type: "list",
+    name: "intro",
+    message: "What type of employee are you adding?",
+    choices: ["Intern", "Manager", "Engineer"],
+  },
+];
 
 const internQuestions = [
   {
     type: "input",
-    name: "employeeName",
+    name: "name",
     message: "What is your name?",
   },
   {
     type: "input",
-    name: "employeeId",
+    name: "id",
     message: "What is your id?",
   },
   {
     type: "input",
-    name: "employeeEmail",
+    name: "email",
     message: "What is your email?",
   },
   {
@@ -38,17 +47,17 @@ const internQuestions = [
 const managerQuestions = [
   {
     type: "input",
-    name: "employeeName",
+    name: "name",
     message: "What is your name?",
   },
   {
     type: "input",
-    name: "employeeId",
+    name: "id",
     message: "What is your id?",
   },
   {
     type: "input",
-    name: "employeeEmail",
+    name: "email",
     message: "What is your email?",
   },
   {
@@ -61,17 +70,17 @@ const managerQuestions = [
 const engineerQuestions = [
   {
     type: "input",
-    name: "employeeName",
+    name: "name",
     message: "What is your name?",
   },
   {
     type: "input",
-    name: "employeeId",
+    name: "id",
     message: "What is your id?",
   },
   {
     type: "input",
-    name: "employeeEmail",
+    name: "email",
     message: "What is your email?",
   },
   {
@@ -81,60 +90,59 @@ const engineerQuestions = [
   },
 ];
 
-const introQuestion = [
-  {
-    type: "checkbox",
-    name: "intro",
-    message: "what type of employee are you adding?",
-    choices: ["Intern", "Manager", "Engineer"],
-  },
-];
-
 const finalQuestion = [
   {
     type: "confirm",
     name: "addMore",
-    message: "do you wish to add another employee?",
+    message: "Do you wish to add another employee?",
   },
 ];
-console.log("you made it 1");
-/*the series that you want is as follows:
-FIRST ask the introQuestion
-SECOND ask the question type relative to that employee type (this is where the if statement comes)
-THIRD push those answers to a master array
-FOURTH ask 
-*/
+
 function askQuestions() {
   inquirer.prompt(introQuestion).then((answers) => {
-    switch (answers) {
-      case answers.intro === "engineer":
+    console.log(answers);
+    switch (answers.intro) {
+      case "Engineer":
         inquirer.prompt(engineerQuestions).then(function (answers) {
-          const engineer = new Engineer(answers);
+          const engineer = new Engineer(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.github
+          );
           employees.push(engineer);
           askFinal();
         });
-
         break;
 
-      case answers.choices === "manager":
+      case "Manager":
         inquirer.prompt(managerQuestions).then(function (answers) {
-          const manager = new Manager(answers);
+          const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.officeNumber
+          );
           employees.push(manager);
           askFinal();
         });
-
         break;
 
-      case answers.choices === "intern":
+      case "Intern":
         inquirer.prompt(internQuestions).then(function (answers) {
-          const intern = new Intern(answers);
+          const intern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.school
+          );
           employees.push(intern);
           askFinal();
         });
         break;
 
       default:
-        console.log("you must select an answer");
+        console.log("You must select an answer");
     }
   });
 }
@@ -144,51 +152,22 @@ function askFinal() {
     if (answers.addMore) {
       askQuestions();
     } else {
+      buildTeam();
       console.log(
-        "thank you so much for your input! the output is in the 'output' folder!"
+        "Thank you so much for your input! The output is in the 'output' folder!"
       );
+
       render(employees);
       return;
     }
   });
 }
 
-// function createEngineer() {
-//   inquirer
-//     .prompt(employeeQuestions, engineerQuestions)
-//     .then(function (engineerAnswers) {
-//       console.log(engineerAnswers);
-//       employees.push(engineerAnswers);
-//       console.log(employees);
-//     });
-// }
-
 askQuestions();
-render(employees);
-//function createManager() {}
-//
-//
 
-// module.exports = response;
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function buildTeam() {
+  fs.writeFile(outputPath, render(employees), (err) => {
+    if (err) throw err;
+    console.log("File Saved");
+  });
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# team-info
+# Team Info
+
+The user story is where a project manager is assembling a team and needs to keep track of who is on the team. The project manager requests an app that can produce ID style placards for each member that can be viewed in the browser. The information entered for each team member will be conducted through the command line interface.
+
+## Getting Started
+
+The user will have to copy the file "team-info" and open it in a code editor such as Visual Studio Code and have to download npm packages including inquirer. Interaction with the app is done through node on the command line. Once started, the user will answer the questions about the employees as they are prompted.
+
+## Tests
+
+The functions in this app were tested during production using the tdd method as well as running the program as a whole to look for bugs and inconsistencies.
+
+## Contributions
+
+If you have any issues with this program or have ideas about making it easier to use please email me at: osiol.anthony@gmail.com
+
+## Acknowledgments
+
+Thanks to Ariel Razon, Justin Pyktel, Jason Novak for providing coding guidance.
